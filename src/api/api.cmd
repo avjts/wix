d14fbea60e883b1ea90f14ef28f435141b0ee008
Andrzej Wojtas <awojtas@logitech.com>
2026-06-05 13:24:29 +0200

    Don't treat warnings as errors
    warn: Package 'System.Security.Cryptography.Xml' 8.0.0 has a known high severity vulnerability

--- src/api/api.cmd
+++ src/api/api.cmd
@@ -23,7 +23,7 @@
 :: Build
 :: Pack
 
-msbuild api_t.proj -p:Configuration=%_C% -tl -nologo -warnaserror -bl:%_L%\api_build.binlog || exit /b
+msbuild api_t.proj -p:Configuration=%_C% -tl -nologo  -bl:%_L%\api_build.binlog || exit /b
 
 :: Test
 dotnet test ^
